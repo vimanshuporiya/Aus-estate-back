a33e0e1c116b87f4640294a06fd45c0cf0ec6b38
vimanshu <Vimanshuporiya123@gmail.com>
2024-08-30 21:21:36 +0530

    CORS : new CORS policy added

--- app.js
+++ app.js
@@ -11,26 +11,39 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8800
-const corsOptions = {
-  origin: [`${process.env.CLIENT_URL}`],
-  methods: "GET,HEAD,PUT,OPTIONS,POST,DELETE",
-  allowedHeaders: [
-    "Access-Control-Allow-Headers",
-    "Origin",
-    "X-Requested-With",
-    "Content-Type",
-    "Accept",
-    "Authorization",
-    "token",
-    "Access-Control-Request-Method",
-    "Access-Control-Request-Headers",
-    "Access-Control-Allow-Credentials",
-  ],
-  credentials: true,
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: [`${process.env.CLIENT_URL}`],
+//   methods: "GET,HEAD,PUT,OPTIONS,POST,DELETE",
+//   allowedHeaders: [
+//     "Access-Control-Allow-Headers",
+//     "Origin",
+//     "X-Requested-With",
+//     "Content-Type",
+//     "Accept",
+//     "Authorization",
+//     "token",
+//     "Access-Control-Request-Method",
+//     "Access-Control-Request-Headers",
+//     "Access-Control-Allow-Credentials",
+//   ],
+//   credentials: true,
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+// };
+// app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+      // res.header("Access-Control-Allow-Origin", "*");
+// , 'https://gamebrag.onrender.com'
+      const allowedOrigins = ['http://localhost:5173', 'https://ausnew.netlify.app'];
+      const origin = req.headers.origin;
+      if (allowedOrigins.includes(origin)) {
+           res.setHeader('Access-Control-Allow-Origin', origin);
+      }
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+      res.header("Access-Control-Allow-credentials", true);
+      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+      next();
+    });
 
 app.use(express.json());
 app.use(cookieParser());
