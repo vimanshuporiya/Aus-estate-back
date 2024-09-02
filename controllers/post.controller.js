a7cd669d3f9acc904733c4fd325d9427bfba1806
vimanshu <Vimanshuporiya123@gmail.com>
2024-09-02 09:39:06 +0530

    Update post.controller.js

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -3,30 +3,30 @@ import jwt from "jsonwebtoken";
 
 export const getPosts = async (req, res) => {
   const query = req.query;
-   console.log(query);
+
   try {
     const posts = await prisma.post.findMany({
       where: {
         city: query.city || undefined,
         type: query.type || undefined,
         property: query.property || undefined,
-        property: query.property || undefined,
         bedroom: parseInt(query.bedroom) || undefined,
         price: {
-          gte: parseInt(query.minPrice) || 0,
-          lte: parseInt(query.maxPrice) || 10000000,
+          gte: parseInt(query.minPrice) || undefined,
+          lte: parseInt(query.maxPrice) || undefined,
         },
       },
     });
 
+    // setTimeout(() => {
+    res.status(200).json(posts);
+    // }, 3000);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get posts" });
+  }
+};
 
-  res.status(200).json(posts);
-
-
-} catch (err) {
-   console.log(err);
-   res.status(500).json({message: "failed to get posts" })
-}} 
 export const getPost = async (req, res) => {
   const id = req.params.id;
   try {
@@ -43,36 +43,30 @@ export const getPost = async (req, res) => {
       },
     });
 
-    let userId;
+    const token = req.cookies?.token;
 
-     const token = req.cookies?.token;
-
-   if (!token) {
-    userId = null;
-   }else{
-    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-      if (err) {
-        userId = null;
-      } else {
-        userId = payload.id
-      }
-    });
-   }
-   const saved = await prisma.savedPost.findUnique({
-    where: {
-      userId_postId:{
-        postId: id,
-        userId,
-      },
-    },
-   });
-
-    res.status(200).json({...post, isSaved: saved ? true : false });
+    if (token) {
+      jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+        if (!err) {
+          const saved = await prisma.savedPost.findUnique({
+            where: {
+              userId_postId: {
+                postId: id,
+                userId: payload.id,
+              },
+            },
+          });
+          res.status(200).json({ ...post, isSaved: saved ? true : false });
+        }
+      });
+    }
+    res.status(200).json({ ...post, isSaved: false });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get post" });
   }
 };
+
 export const addPost = async (req, res) => {
   const body = req.body;
   const tokenUserId = req.userId;
@@ -87,39 +81,42 @@ export const addPost = async (req, res) => {
         },
       },
     });
-
     res.status(200).json(newPost);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to create post" });
   }
 };
-export const updatePosts = async (req, res) => {
-  try {
-    const posts = await prisma.post.findMany();
 
-    res.status().json();
+export const updatePost = async (req, res) => {
+  try {
+    res.status(200).json();
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "Failed to Update post" });
+    res.status(500).json({ message: "Failed to update posts" });
   }
 };
+
 export const deletePost = async (req, res) => {
   const id = req.params.id;
   const tokenUserId = req.userId;
+
   try {
     const post = await prisma.post.findUnique({
       where: { id },
     });
+
     if (post.userId !== tokenUserId) {
-      return res.status(403).json({ message: "Not Authorized" });
+      return res.status(403).json({ message: "Not Authorized!" });
     }
+
     await prisma.post.delete({
       where: { id },
     });
-    res.status(200).json({ message: "Post Deleted" });
+
+    res.status(200).json({ message: "Post deleted" });
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "Failed to Delete post" });
+    res.status(500).json({ message: "Failed to delete post" });
   }
 };
